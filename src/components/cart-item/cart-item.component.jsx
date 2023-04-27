1d7ba86d0f307abcce7f64d09375a4dc86cf341e
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2023-04-27 10:36:04 -0400

    refactoring to styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,13 +1,13 @@
-import "./cart-item.styles.scss";
+import './cart-item.styles.jsx';
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={name} />
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price">
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
           {quantity} x ${price}
         </span>
       </div>
